873d79ce1214be9fee991dbec7e8fd133b06284c
Tatiana Manakhova <tat.manakhova@gmail.com>
2021-04-20 23:55:45 +0300

    Создает презентеры

---
+++ src/presenter/event.js
@@ -0,0 +1,113 @@
+import EventView from '../view/event';
+import EventEditView from '../view/event-edit';
+import {render, RenderPosition, replace, remove} from '../utils/render';
+
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
+export default class Event {
+  constructor(eventListContainer, changeData, changeMode) {
+    this._eventListContainer = eventListContainer;
+    this._changeData = changeData;
+    this._changeMode = changeMode;
+
+    this._eventComponent = null;
+    this._eventEditComponent = null;
+    this._mode = Mode.DEFAULT;
+
+    this._handleEditClick = this._handleEditClick.bind(this);
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+    this._handleCloseEditClick = this._handleCloseEditClick.bind(this);
+    this._escKeyDownHandler = this._escKeyDownHandler.bind(this);
+    this._handleFavoriteClick = this._handleFavoriteClick.bind(this);
+  }
+
+  init(event) {
+    this._event = event;
+
+    const prevEventComponent = this._eventComponent;
+    const prevEventEditComponent = this._eventEditComponent;
+
+    this._eventComponent = new EventView(event);
+    this._eventEditComponent = new EventEditView(event);
+
+    this._eventComponent.setEditClickHandler(this._handleEditClick);
+    this._eventEditComponent.setFormSubmitHandler(this._handleFormSubmit);
+    this._eventEditComponent.setCloseEditClickHandler(this._handleCloseEditClick);
+    this._eventComponent.setFavoriteClickHandler(this._handleFavoriteClick);
+
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this._eventListContainer, this._eventComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this._mode === Mode.DEFAULT) {
+      replace(this._eventComponent, prevEventComponent);
+    }
+
+    if (this._mode === Mode.EDITING) {
+      replace(this._eventEditComponent, prevEventEditComponent);
+    }
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
+  }
+
+  destroy() {
+    remove(this._taskComponent);
+    remove(this._taskEditComponent);
+  }
+
+  resetView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceFormToEvent();
+    }
+  }
+
+  _replaceEventToForm() {
+    replace(this._eventEditComponent, this._eventComponent);
+    document.addEventListener('keydown', this._escKeyDownHandler);
+    this._changeMode();
+    this._mode = Mode.EDITING;
+  }
+
+  _replaceFormToEvent() {
+    replace(this._eventComponent, this._eventEditComponent);
+    document.removeEventListener('keydown', this._escKeyDownHandler);
+    this._mode = Mode.DEFAULT;
+  }
+
+  _escKeyDownHandler(evt) {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this._replaceFormToEvent();
+    }
+  }
+
+  _handleEditClick() {
+    this._replaceEventToForm();
+  }
+
+  _handleCloseEditClick() {
+    this._replaceFormToEvent();
+  }
+
+  _handleFormSubmit(event) {
+    this._changeData(event);
+    this._replaceFormToEvent();
+  }
+
+  _handleFavoriteClick() {
+    this._changeData(
+      Object.assign(
+        {},
+        this._event,
+        {
+          isFavorite: !this._event.isFavorite,
+        },
+      ),
+    );
+  }
+}
--- src/utils/common.js
+++ src/utils/common.js
@@ -11,4 +11,16 @@ export const shuffleArray = (array) => {
   return items;
 };
 
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
 
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
